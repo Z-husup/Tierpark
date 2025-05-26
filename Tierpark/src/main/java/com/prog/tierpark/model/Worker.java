a911d9a20e8c99eed27e7bcbae9873a7d0926d52
Zhusup Akylbek <jojomen0039@gmail.com>
2025-05-26 20:15:39 +0200

    Add FXML views and adjust Worker model for new UI features
    Introduced multiple FXML views for admin, worker, and login interfaces, enhancing UI navigation and functionality. Replaced `firstName` and `familyName` fields with `fullName` in the Worker model to simplify data handling. Added supporting resources such as `worker-logo.jpg`.

--- Tierpark/src/main/java/com/prog/tierpark/model/Worker.java
+++ Tierpark/src/main/java/com/prog/tierpark/model/Worker.java
@@ -9,12 +9,10 @@ import java.time.LocalDate;
 @Data
 public class Worker {
     private Long id;
-    private String firstName;
-    private String familyName;
+    private String fullName;
     private String email;
     private String phoneNumber;
     private LocalDate dateOfBirth;
-    private int age;
     private String gender;
     private LocalDate hireDate;
     private Enclosure enclosure;
